9bfcecb6f3a12e84a1c4f6ff3c71b44cffedbbb4
NadyLuna <nadyedja@gmail.com>
2020-11-14 18:01:53 -0300

    Inclusão de Rotas

--- src/controller/livroController.js
+++ src/controller/livroController.js
@@ -25,6 +25,20 @@ const getById = (req, res) => {
     });
 };
 
+const topFive = (req, res) => {
+    livros.find({ topFive: true },((err, livros) => {
+            if (err) {
+                res.status(500).send({message: err.message});
+            } else{
+                res.status(200).send(livros);
+                }     
+        })  
+    );
+};
+
+    
+
+
 const postLivros = (req, res) => {
     livros.countDocuments((err, count) => {
         if (err) {
@@ -44,10 +58,49 @@ const postLivros = (req, res) => {
                 });
             };
         });
-    };
+};
+
+
+const deleteLivro = (req, res) => {
+        const id = req.params.id;
+        livros.find({id}, (err, livro) => {
+            if (livro.length > 0) {
+                livros.deleteOne({id}, (err) => {err?
+                        res.status(424).send({message: err.message}) :
+                        res.status(200).send({status: true, mensagem: "livro excluído com sucesso", });
+                });
+            } else {
+                res.status(404).send("livro não encontrado");
+            }
+        });
+};
+
+
+const putLivro = (req, res) => {
+        const id = req.params.id;
+        livros.updateMany({ id }, {$set: req.body}, {upsert: true}, function ( err) {
+            if (err) {
+                res.status(500).send({message: err.message});
+            } else {
+                res.status(200).send({message: "livro atualizado com sucesso"});
+            }
+        });
+};
+    console.log(req.url);
+    livros.find(function (err, livros) {
+        if (err)
+            res.status(500).send({message: err.message,});
+
+        res.status(200).send(livros);
+});
+
+
 
 module.exports = {
     getAll,
-    getById,
-    postLivros
+    topFive,
+    getById,    
+    postLivros,
+    deleteLivro,
+    putLivro
 };
--- src/routes/livroRouter.js
+++ src/routes/livroRouter.js
@@ -3,9 +3,10 @@ const router = express.Router();
 const controller = require('../controller/livroController');
 
 router.get('/', controller.getAll)
+router.get('/', controller.topFive)
 router.get('/:id', controller.getById)
 router.post('/livro', controller.postLivros)
-//router.delete('/:id', controller.deleteLivro)
+router.delete('/:id', controller.deleteLivro)
 //router.get('/:genero', controller.getLivrosByGenero)
 //router.put('/:id', controller.putLivro)
 //router.patch('/:id', controller.patchLivro)
